ca3037298372c9ec0ef2c6d2edc58e7209d7eec5
Cameron Shatto <cameronshatto99@gmail.com>
2024-09-19 16:12:26 -0500

    fix(dev): fix vitest config

--- src/utils/exampleUtil/index.test.ts
+++ src/utils/exampleUtil/index.test.ts
@@ -1,5 +1,3 @@
-import { describe, expect, it } from 'vitest';
-
 describe('test-suite', () => {
   it('should pass', () => {
     expect(true).toBe(true);
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
+    globals: true,
     include: ['src/**/*.test.{ts,tsx}'],
     coverage: {
       provider: 'v8',
